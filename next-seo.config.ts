fc43f44b8aab61d995e04423f939854d6ae224d7
Ricardo Costa <rcostaj00@gmail.com>
2022-01-13 15:06:50 -0300

    def_commit

---
+++ next-seo.config.ts
@@ -0,0 +1,13 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://github.com/ricardo12377',
+    site_name: 'My Trips'
+  },
+  twitter: {
+    handle: '@https://github.com/ricardo12377',
+    site: '@site',
+    cardType: 'summary_large_image'
+  }
+}
